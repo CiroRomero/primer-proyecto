6c87b65d11e691f96d9948be63750fb66e2cb1af
CiroRomero <rccirofacondo@gmail.com>
2024-04-19 11:05:10 -0300

    agregue el navbar y le agregue rutas- hice algunas cards y puse un carrusel (todo es temporal hasta que se me ocurra una idea)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"inicio",component:InicioComponent},
+  {path:"galeria",component:GaleriaComponent},
+  {path:"carrusel",component:CarruselComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
